84c34475baea9d8335d906e4b2f06018d91a0477
RoshanRane <rosh1992@gmail.com>
2023-05-05 14:59:41 +0200

    working image generator
    first version. Doesn't have any covariates or labels.

--- create_shapes_data.py
+++ create_shapes_data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 import pandas as pd
 from matplotlib import pyplot as plt
 plt.rcParams['axes.facecolor']='black'
@@ -14,12 +15,12 @@ from tqdm import tqdm
 import argparse
 
 
-class _gen_param:
+class _GEN_VAR:
     def __init__(self, name, states):
         self.name = name
         self.states = states
         self.k = len(states)
-        self.weights = np.ones(self.k)
+        self.reset_weights()
         
     def bump_up_weight(self, i, amt=1):
         self.weights[i] += amt
@@ -44,13 +45,15 @@ class _gen_param:
         
     def sample(self):
         probas = self.weights/self.weights.sum()
-        return np.random.choice(self.states, p=probas)
-
+        return np.random.choice(self.states, p=probas).item()
+    
+    def reset_weights(self):
+        self.weights = np.ones(self.k)
     
     
 class ShapesData:
     
-    def __init__(self, out_dir="./shapes/", img_size=64):
+    def __init__(self, out_dir="./shapes/", img_size=64, seed=None):
         self.I = img_size
         self.OUT_DIR = out_dir
         self.IMGS_DIR = f'{self.OUT_DIR}/images/'
@@ -60,94 +63,140 @@ class ShapesData:
         os.makedirs(self.IMGS_DIR, exist_ok=True)
         os.makedirs(self.LBLS_DIR, exist_ok=True)
         
+        if seed is not None:
+            np.random.seed(seed)
+            random.seed(seed)
+        # the center of the image for drawing reference
+        self.ctr = (self.I/2, self.I/2) #np.random.randint(self.I/2-2,self.I/2+2, size=2)
+        
+        # define all the generative properties for the images
+        self.GEN_PROPS = {
+            # 1. brain_vol: total volume of the brain pi*radius_minor*radius_major
+            # ranging between 1633 to 2261 [(S/2-12)*(S/2-6) to (S/2-8)*(S/2-2)]
+            'brain_vol-radminor':_GEN_VAR('brain_vol-rad-minor', 
+                                       np.arange(self.I/2 - 12, self.I/2 - 8 + 1, dtype=int)),
+            'brain_vol-radmajor':_GEN_VAR('brain_vol-rad-major', 
+                                       np.arange(self.I/2 - 6, self.I/2 - 2 + 1, dtype=int)),                        
+            # 2. brain_thick: the thickness of the blue border around the brain ranging between 1 to 4
+            'brain_thick':_GEN_VAR('brain_thick', np.arange(1,4+1, dtype=int)), 
+            # 3. the intensity or brightness of the brain region ranging between 'greyness1' (210) to 'greyness5' (170)
+            'brain_int':_GEN_VAR('brain_int', [210,200,190,180,170]), 
+            # 4. the intensity or brightness of the ventricles and brain borders ranging between 'blueness1' to 'blueness3' 
+            'border_int':_GEN_VAR('border_int', ['slateblue','mediumslateblue','darkslateblue']),
+            # ventricle (the 2 touching arcs in the center) thickness ranging between 1 to 4
+            'vent_thick':_GEN_VAR('vent_thick', np.arange(1,4+1, dtype=int)),
+            # 'vent_curv' (TODO) curvature of the ventricles ranging between ..
+        }
+                                
+        # also add the gen props of the 5 shapes 
+        self.SHAPE_POS = {'shape-top': (self.I*.5, self.I*.22),
+                          'shape-midr':(self.I*.7, self.I*.4),
+                          'shape-midl':(self.I*.3, self.I*.4),
+                          'shape-botr':(self.I*.4, self.I*.7),
+                          'shape-botl':(self.I*.6, self.I*.7)}
+                                          
+        for shape_pos in self.SHAPE_POS.keys():
+            self.GEN_PROPS.update({
+                # number of sides in the regular polygon from 3 (triangle) to 12
+                f'{shape_pos}_curv': _GEN_VAR(f'{shape_pos}_curv',
+                                              np.arange(3,12, dtype=int)), 
+                # color of the regular polygon from shades of green to shades of red
+                f'{shape_pos}_int': _GEN_VAR(f'{shape_pos}_int', 
+                                            ['lightpink','lightsalmon','peach',
+                                             'rose','lightseagreen','lightgreen']),  #TODO
+                # the radius of the circle inside which the regular polygon is drawn
+                f'{shape_pos}_vol-rad': _GEN_VAR(f'{shape_pos}_volrad', 
+                                            np.arange(2,5+1, dtype=int)),  
+                # TODO # rot = np.random.randint(0,360)
+            })
+        
     
     def generate_dataset(self, n_samples):
         """Creates toy dataset and save to disk."""
         # first define the generative parameters of the figure
         self.df =  pd.DataFrame()
-        self.df.index.name = "subjectID"
-        
-        # generative properties of the images that can be controlled
-        for gen_param in ['brain_vol', # total volume of the brain ranging between 1633 to 2261 [(S/2-12)*(S/2-6) to (S/2-8)*(S/2-2)]
-                        'brain_thick', # the thickness of the blue border around the brain ranging between 1 to 4
-                        'brain_int', # the intensity or brightness of the brain region ranging between 'greyness0' to 'greyness4'
-                        'border_int', # the intensity or brightness of the ventricles and brain borders ranging between 'blueness0' to 'blueness3' 
-                        'vent_thick', # ventricle (the 2 touching arcs in the center) thickness ranging between 1 to 4
-                        # 'vent_curv', # curvature of the ventricle ranging between 
-                        'frontal_shape', 'frontal_thick', 'frontal_color', 'frontal_vol',
-                        'caudal_shape', 'caudal_thick', 'caudal_color', 'caudal_vol']:
-            self.df[gen_param] = np.nan
+        self.df.index.name = "subjectID"        
+        for gen_var_name, gen_var in self.GEN_PROPS.items():
+            self.df[gen_var_name] = np.nan
+            
+        print("Generating {} synthetic 'toy brain' images:".format(n_samples))
 
-        print('Generating {} sythetic images:'.format(n_samples))
-
-        for i in tqdm(range(n_samples)):
-            # Create a new image of size 64x64 with a black background
+        for subID in tqdm(range(n_samples)):
+            # reset the distribution of the generative properties of the image generations
+            [gen_var.reset_weights() for _, gen_var in self.GEN_PROPS.items()]
+            props = {}
+                
+            # (1) sample the convariates / confounders for the data point
+            ## TODO
+                
+            # Create a new image of size 64x64 with a black background and a draw object on it
             image = Image.new('RGB',(self.I,self.I),(0,0,0))
-            # Create a new drawing object
             draw = ImageDraw.Draw(image)
 
-            # (1) Draw the whole brain 
-            ctr = (self.I/2, self.I/2) #np.random.randint(self.I/2-2,self.I/2+2, size=2)
-            rd = np.array([np.random.randint(self.I/2-12,self.I/2-8), np.random.randint(self.I/2-6,self.I/2-2)])
-            thick = np.random.randint(1,4)
-            color  = np.random.choice([210,200,190,180,170], p=[1/5,1/5,1/5,1/5,1/5])
-            outline_color = np.random.choice(['slateblue','mediumslateblue','darkslateblue'])
-            # save the whole brain generative properties
-            self.df.loc[i, 'brain_vol'] = np.around(math.pi*np.prod(rd))
-            self.df.loc[i, 'brain_color'] = color
-            self.df.loc[i, 'brain_thick'] = thick
-            self.df.loc[i, 'border_int'] = outline_color
+            # (2) Draw the whole brain 
+            # sample and save the whole brain generative properties
+            for gen_var_name, gen_var in self.GEN_PROPS.items():
+                if ('brain_' in gen_var_name) or (gen_var_name=='border_int'):
+                    props.update({gen_var_name: gen_var.sample()})
 
-            # draw an ellipse
-            x0,y0 = (ctr[0]-rd[0]  ,ctr[1]-rd[1]  )
-            x1,y1 = (ctr[0]+rd[0]-1,ctr[1]+rd[1]-1)
+            # (2a) Draw an outer ellipse
+            x0,y0 = (self.ctr[0]-props['brain_vol-radminor'],
+                     self.ctr[1]-props['brain_vol-radmajor'])
+            x1,y1 = (self.ctr[0]+props['brain_vol-radminor']-1,
+                     self.ctr[1]+props['brain_vol-radmajor']-1)
+                
             draw.ellipse((x0,y0,x1,y1), 
-                         fill=self.get_color_val(color), 
-                         width=thick, outline=self.get_color_val(outline_color))
+                         fill=self.get_color_val(props['brain_int']), 
+                         width=props['brain_thick'], 
+                         outline=self.get_color_val(props['brain_int']))
             # save the brain mask
-            brain_mask = (np.array(image).sum(axis=-1) > 0)
+            brain_mask = (np.array(image).sum(axis=-1) > 0) #TODO save it
 
-            # Draw ventricles as 2 touching arcs 
-            vent_thick=np.random.randint(1,4)
+            # (2b) Draw ventricles as 2 touching arcs 
+            for gen_var_name, gen_var in self.GEN_PROPS.items():
+                if ('vent_' in gen_var_name):
+                    props.update({gen_var_name: gen_var.sample()})
 
             xy_l = (self.I*.3, self.I*.3, self.I*.5, self.I*.5)
             xy_r = (self.I*.5, self.I*.3, self.I*.7, self.I*.5)
             draw.arc(xy_l, start=+310, end=+90, 
-                     fill=self.get_color_val('slateblue'), width=vent_thick)
+                     fill=self.get_color_val(props['border_int']), 
+                     width=props['vent_thick'])
             draw.arc(xy_r, start=-290, end=-110, 
-                     fill=self.get_color_val('slateblue'), width=vent_thick)
-            # store ventricle generative props
-            self.df.loc[i, 'vent_thick'] = vent_thick
+                     fill=self.get_color_val(props['border_int']), 
+                     width=props['vent_thick'])
 
-            # draw shapes (triangle, square, pentagon, hexagon, ..)
-            # of different size, color and rotations
-            regions = {'top': (self.I*.5,self.I*.22),
-                       'mid_right':(self.I*.7,self.I*.4), 'mid_left':(self.I*.3,self.I*.4),
-                       'bot_right':(self.I*.4,self.I*.7), 'bot_left':(self.I*.6,self.I*.7)}
-
-            for region,(x,y) in regions.items():
-
-                n_sides = np.random.randint(3,15)
-                # rot = np.random.randint(0,360)
-                color = np.random.choice(['lightpink', 'lightsalmon', 
-                                          'peach', 'rose', 
-                                          'lightseagreen', 'lightgreen'])
-                # thick = np.random.randint(1,3)
-                r = np.random.uniform(2,5)
-                draw.regular_polygon((x,y,r), n_sides, #rotation=rot, 
-                                     fill=self.get_color_val(color), 
-                                     outline=self.get_color_val(outline_color))
-
-            # Save the image
-            image.save(f"{self.IMGS_DIR}{i:05}.jpg")
-            
-            # save the data csv every 1000 samples
-            if i//1000 == 0:
-                self.df.to_csv(f"{self.OUT_DIR}/toybrains_n{n_samples}.csv")
+            # (3) draw 5 shapes (triangle, square, pentagon, hexagon, ..)
+            # with different size, color and rotations
+            for shape_pos, (x,y) in self.SHAPE_POS.items():
                 
+                for gen_var_name, gen_var in self.GEN_PROPS.items():
+                    if (shape_pos in gen_var_name):
+                        props.update({gen_var_name: gen_var.sample()})
+                        
+                
+                draw.regular_polygon((x,y,props[f'{shape_pos}_vol-rad']), 
+                                     n_sides=props[f'{shape_pos}_curv'], 
+                                     rotation=np.random.randint(0,360),
+                                     fill=self.get_color_val(props[f'{shape_pos}_int']), 
+                                     outline=self.get_color_val(props['border_int']))
+            
+            # store the generative properties
+            self.df.loc[f'{subID:05}'] = props 
+            # save the data csv every 1000 samples
+            if subID//1000 == 0:
+                self.df.to_csv(f"{self.OUT_DIR}/toybrains_n{n_samples}.csv")      
+            
+            # Save the image
+            image.save(f"{self.IMGS_DIR}{subID:05}.jpg")
+                
+        # format the subject IDs same as the filename
         self.df.to_csv(f"{self.OUT_DIR}/toybrains_n{n_samples}.csv")
         
         
+            # 'brain_vol_states',math.pi*np.meshgrid(gen_rad_width.states, gen_rad_height.states),
+            # 'brain_vol',_GEN_VAR('brain_vol', 
+            #                      brain_vol_states),   
         
         
     def get_color_val(self, color):
