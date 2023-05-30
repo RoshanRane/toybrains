808d4ed07a2138bc58e8db5d82d374f61d3bd721
RoshanRane <rosh1992@gmail.com>
2023-05-30 14:26:58 +0200

    plot distribution by covariates

--- helper/viz_helpers.py
+++ helper/viz_helpers.py
@@ -11,6 +11,7 @@ from tqdm.notebook import tqdm
 import random
 import math
 import json
+import matplotlib as mpl
 from  matplotlib.ticker import FuncFormatter
 from matplotlib.colors import is_color_like
 from colordict import ColorDict, rgb_to_hex
@@ -37,12 +38,12 @@ should either me a string path to the image files or numpy arrays"
     plt.show()
 
 
-def plot_col_dists(df, title=''):
+def plot_col_counts(df, title=''):
     
-    # convert column to appropriate dtype and then decide the plottype
     df_copy = df.reindex(sorted(df.columns), axis=1).copy()
     cols = df_copy.columns 
-
+    # convert each column to its appropriate dtype and 
+    # then decide the type of plot to use for it
     plottypes = {}
     for col in cols:
         if df_copy[col].nunique()==2:
@@ -98,8 +99,8 @@ def plot_col_dists(df, title=''):
                 ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
                 
         elif plottype == 'hist':
-            bins = 10 if df_copy[col].nunique()>10 else df_copy[col].nunique()
-            sns.histplot(data=df_copy, x=col, bins=, ax=ax)
+            bins = df_copy[col].nunique()//4# if df_copy[col].nunique()>10 else df_copy[col].nunique()
+            sns.histplot(data=df_copy, x=col, ax=ax, kde=True) #multiple='fill'
         elif plottype == 'pie':
             cnt = df_copy[col].value_counts().sort_index()
             ax.pie(cnt, labels=cnt.index,
@@ -109,3 +110,49 @@ def plot_col_dists(df, title=''):
         ax.set_xlabel(None)
 
     plt.tight_layout()
+    
+    
+def plot_col_dists(df, attr_cols, cov_cols, title=''):
+    df = df.copy()
+    attr_cols = sorted(attr_cols)
+    cov_cols = sorted(cov_cols)
+    subplot_nrows = len(attr_cols)
+    subplot_ncols = len(cov_cols)
+    fs=12
+    # convert all columns to numerical
+    for col in attr_cols:
+        if not np.issubdtype(df[col].dtype, np.number):
+            vals =  {v:i for i,v in enumerate(sorted(df[col].unique()))}
+            df[col] = df[col].map(vals)
+            
+    # if the covariates are continuous then bin them
+    for col in cov_cols:
+        if df[col].nunique()>5:
+            df[col] = pd.cut(df[col], bins=3, precision=0)
+    
+    # create subplots set attributes
+    f,axes = plt.subplots(subplot_nrows, subplot_ncols, 
+                          figsize=(2+2*subplot_ncols, 2*subplot_nrows),
+                          constrained_layout=True
+                          )
+    
+    legends = []
+    for i, axis_row in enumerate(axes):
+        for j, ax in enumerate(axis_row):
+            attr, cov = attr_cols[i], cov_cols[j]
+            g = sns.kdeplot(df, x=attr, hue=cov, ax=ax, fill=True, legend=(i==0))
+            if i==0: 
+                sns.move_legend(g, "upper center", bbox_to_anchor=(0.5,1.5), 
+                                alignment='center', ncols=2,
+                                title_fontproperties={'size':fs}) #, 'weight':'heavy'
+            # g._legend.set_bbox_to_anchor((1.2,1.5))
+            # set xlabel and ylabel at the top and leftside of the plots resp.
+            ax.set_ylabel(attr, fontsize=fs) if j==0 else ax.set_ylabel(None)
+            ax.set_xlabel(None)
+            # if i==0: ax.set_title(f"covariate = {cov.replace('cov_','')}")
+        
+    if title: f.suptitle(title, fontsize=fs+4)
+    f.supylabel("Density")
+    f.supylabel("Covariates = ")
+    
+    # plt.tight_layout()
