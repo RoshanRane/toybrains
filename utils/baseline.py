4b2f55cbcad966eb8ea19902a3d0f0546a9c56ba
jihoonkim2100 <kimjihoon2100@gmail.com>
2023-09-18 16:07:47 +0200

    add visualization
    add visualization, and summary

--- utils/baseline.py
+++ utils/baseline.py
@@ -6,8 +6,10 @@ from datetime import datetime
 from collections import Counter
 from tqdm import tqdm
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
+import seaborn as sns
 import subprocess
 from joblib import Parallel, delayed
 
@@ -16,6 +18,7 @@ from utils.dataset import generate_dataset
 from utils.tabular import get_table_loader, run_lreg
 
 sys.path.insert(0, "../")
+sns.set_theme(style="ticks", palette="pastel")
 
 # functions
 
@@ -280,3 +283,134 @@ def run_baseline_pipeline(
     runtime = str(datetime.now()-start_time).split(".")[0]
     print(f'TOTAL PIPELINE RUNTIME: {runtime}')
     return out_path_list
+
+# visualization
+def viz_baseline(
+    output_list,
+):
+    ''' vizualization output
+    
+    PARAMETER
+    ---------
+    output_list : list
+        run.csv of the output_list
+    
+    TODO add parameters
+    
+    '''
+    num = len(output_list)
+    if len(output_list) == 1:
+        RUN = pd.read_csv(output_list[0])
+    if len(output_list) != 1:
+        DFS = [pd.read_csv(output_csv) for output_csv in output_list]
+        RUN = pd.concat(DFS)
+    
+    viz_df = RUN.copy(deep=True)
+    
+    x = 'test_metric'
+    y = 'out'
+    hue = 'inp'
+    y_order = ['lblbin_shp', 'lblbin_shp-vent', 'lblbin_shp-vol', 'cov_sex', 'cov_site', 'cov_age']
+    hue_order = ['a', 'c', 'a+c']
+    datasets = list(viz_df['dataset'].unique())
+    num_rows = len(datasets)
+    join = False
+
+    # setup the figure properties
+    sns.set(style='whitegrid', context='paper')
+    fig, axes = plt.subplots(num_rows, 1,
+                             sharex=True, sharey=True,
+                             dpi=120, figsize=(7,5))
+
+    if num_rows == 1: axes = [axes]
+    plt.xlim([-0.1, 1.1])
+
+    for ax, dataset in zip(axes, datasets):
+        dfn = viz_df.query(f"dataset == '{dataset}'")
+
+        # plotting details
+        palette = sns.color_palette()
+        dodge, scale, errwidth, capsize = 0.4, 0.4, 0.9, 0.08
+    
+        ax = sns.pointplot(y=y, x=x, order=y_order,
+                           hue=hue, hue_order=hue_order,
+                           join=join, data=dfn, ax=ax,
+                           errorbar='sd', errwidth=errwidth, capsize=capsize,
+                           dodge=dodge, scale=scale, palette=palette)
+    
+        ax.legend_.remove()
+        ax.set_title(f"{dataset}")
+        ax.set_xlabel("")
+        ax.set_ylabel("")
+    
+        # draw the chance line in the legend
+        chance = [0, 0.25, 0.5, 0.5, 0.5, 0.5]
+        for z, ch in enumerate(chance):
+            # print(f'z={z}, ch={ch}'
+            # print(f'ymin={z/len(chance)}, ymax={(z+1)/(len(chance))}')
+            ax.axvline(x=ch, ymin=z/len(chance), ymax=(z+1)/(len(chance)), label="chance", c='gray', ls='--', lw=1.5)
+        ax.axhline(y=3.5, xmin=0.29, xmax=0.5, c='gray', ls='--', lw=1.5)
+        ax.axhline(y=4.5, xmin=0.09, xmax=0.29, c='gray', ls='--', lw=1.5)
+
+    # set custom x-axis tick positions and labels
+    x_tick_positions = [0, 0.2, 0.4, 0.6, 0.8, 1.0]
+    x_tick_labels = ['0', '20', '40', '60', '80', '100']
+
+    for ax in axes:
+        ax.set_xticks(x_tick_positions)
+        ax.set_xticklabels(x_tick_labels)
+
+    for ax in axes[:-1]:
+        ax.set_xlabel("")
+
+    # add labels for the last subplot
+    axes[-1].set_xlabel(f"{x} - Accuracy (%)")
+
+    legend_handles = []
+
+    legend_labels = ["Attributes (A)", "Covariates (C)", "A+C"]
+    for i, label in enumerate(legend_labels):
+        legend_handles.append(plt.Line2D([0], [0], marker='', linestyle='-', color=palette[i], label=label))
+
+    # add the legend outside the subplots
+    plt.legend(handles=legend_handles, loc='upper right', title='Inputs', fontsize=7, bbox_to_anchor=(1.0, 0.4))
+    
+    plt.suptitle("Baseline Analysis Plot")
+    plt.tight_layout()
+    plt.show()
+    # plt.savefig("figures/results_bl.pdf", bbox_inches='tight')
+
+    return viz_df
+    
+# summary
+def summary(
+    df,
+    col=None,
+    cmap='YlOrBr',
+):
+    ''' summary 
+    
+    PARAMETER
+    ---------
+    DF : pandas.dataframe
+        run.csv
+        
+    col : None or string
+        target columns if None then display all the metric
+    '''
+    assert col in [None, 'train_metric', 'val_metric', 'test_metric']
+    
+    desc = pd.DataFrame(df.groupby(['out', 'inp', 'dataset'])['train_metric', 'val_metric', 'test_metric'].describe())
+    if col is None:
+        desc = desc[
+            [('train_metric', 'mean'), ('train_metric', 'std'), ('train_metric', 'min'), ('train_metric', 'max'),
+             ('val_metric', 'mean'), ('val_metric', 'std'), ('val_metric', 'min'), ('val_metric', 'max'),
+             ('test_metric', 'mean'), ('test_metric', 'std'), ('test_metric', 'min'), ('test_metric', 'max')]]
+    else:
+        desc['average'] = desc[(col, 'mean')].values
+        desc['standard_deviation'] = desc[(col, 'std')].values
+        desc['min'] = desc[(col, 'min')].values
+        desc['max'] = desc[('train_metric', 'max')].values
+        desc = desc[['average', 'standard_deviation', 'min', 'max']]
+        
+    return desc.style.background_gradient(cmap=cmap)
